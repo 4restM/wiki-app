7f38fc57509db81f9db14316a6e95719fab71b31
Forrest Miller <forrest.w.miller@icloud.com>
2024-10-25 19:58:24 -0500

    feat: History and Search component init

---
+++ frontend/src/components/History.jsx
@@ -0,0 +1,9 @@
+const History = () => {
+  return (
+    <div>
+      <p>FILL ME IN</p>
+    </div>
+  );
+};
+
+export default History;
---
+++ frontend/src/components/Search.jsx
@@ -0,0 +1,9 @@
+const Search = () => {
+  return (
+    <div>
+      <p>FILL ME IN</p>
+    </div>
+  );
+};
+
+export default Search;
