d3506ea49f30a5ab8a181a49ed6e029d88614c48
Forrest Miller <forrest.w.miller@icloud.com>
2024-10-26 04:06:18 -0500

    feat: migrations and seeder init

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+const { sequelize } = require("./models");
 //MW
 app.use(express.json());
 
 //Routes
 const searchRoutes = require("./routes/searchRoutes");
 const historyRoutes = require("./routes/historyRoutes");
+
+//sync db on start
+sequelize
+  .authenticate()
+  .then(() => console.log("database connected"))
+  .catch((err) => console.log("error: " + err));
+
+sequelize.sync({ alter: true });
 
 app.listen(PORT, () => {
   console.log(`Server is up on http://localhost:${PORT}`);
